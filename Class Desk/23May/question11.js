8b42f4fd0bd00994b9fbd21ded73b35254c99536
Aman burnwal <aman1@mailfence.com>
2022-07-06 09:51:59 +0530

    fibonacchi series

---
+++ Class Desk/23May/question11.js
@@ -0,0 +1,28 @@
+// Create Fibonacci series of 5 and 8.
+
+// Hint : The Fibonacci sequence is a series of numbers in which each number is the sum of the two that
+// precede it. Starting at 0 and 1, the sequence looks like this: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, and so on
+// forever.    
+
+function nthFibonachiSeries(num){
+    let firstNum = 0;
+    let secondNum = 1;
+    let nthFibonachi;
+
+    if(num === 1 || num === 0){
+        return 0;
+    }
+
+    if(num === 2){
+        return 1;
+    }
+
+    for(let i = 3; i <= num; i++){
+        nthFibonachi = firstNum + secondNum;
+        firstNum = secondNum
+        secondNum = nthFibonachi;
+    }
+    return nthFibonachi;
+}
+
+console.log(nthFibonachiSeries(6))
