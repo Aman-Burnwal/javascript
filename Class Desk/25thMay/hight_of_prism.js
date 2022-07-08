c51998bdc4f20ffc7a8576437cc629a5139c399a
Aman burnwal <aman1@mailfence.com>
2022-07-08 22:50:55 +0530

    hight of prism

---
+++ Class Desk/25thMay/hight_of_prism.js
@@ -0,0 +1,34 @@
+/*Problem Statement:
+    Given the volume and perimeter of the triangular base of the prism, and the circumference of the
+    inner circle. Calculate the height of the prism.
+Example:
+
+Input 
+    radius=3, perimeter=15, Volume=270
+
+Output 
+    12
+
+Explanation:
+
+    The radius of the triangle is given as ` area / semi perimeter `. And the semi perimeter of the triangle is
+    half the triangle perimeter. Substituting the values we can get the area of the triangle i.e.
+
+
+    area = Inradius * semi perimeter
+        = 3  *  15  /  2
+
+
+    Comparing it with volume of prism i.e. area * h we can get the height as
+
+            270 = h * ( 3 * 15 / 2 )
+              h = 12
+
+*/
+
+
+function heigh_of_prism(perimeter, radius, volume){
+    
+}
+
+
