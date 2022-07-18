2b92b146d120381797d39d0f28f468ec105fa551
Aman burnwal <aman1@mailfence.com>
2022-07-18 20:58:36 +0530

    objective question

---
+++ ReLevel By Unacademy/Class Desk/31May/Data Structures Arrays and Objects (Part-1)/output_Guess.js
@@ -0,0 +1,14 @@
+const car = {
+    color: 'Black',
+    speed: '120Kmph',
+    brand: 'Audi',
+    start: function () {
+        console.log('Car started');
+    },
+    stop: function () {
+        console.log('Car stopped');
+    },
+}
+const newCar = car;
+newCar.brand = 'BMW';
+console.log(car.brand);
