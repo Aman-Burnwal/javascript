65ada2582190c6d7cd1117557f349a496add167d
Aman burnwal <aman1@mailfence.com>
2022-07-21 15:19:14 +0530

    maggic square

---
+++ ReLevel By Unacademy/Assignments/Array/maggic_square.js
@@ -0,0 +1,119 @@
+// 2)	
+// Given 2D Array. A Magic square is a 2D array having n rows and n columns which will be having all
+// elements as distinct and the sum of row, column or diagonal is equal to the same number.
+
+// You need to check whether that array is magic square or not.
+
+// Input -> 
+
+const array = [[2, 7, 6],
+[9, 5, 1],
+[4, 3, 8]]
+console.log(isArraySquareMagic(array));
+
+function isArraySquareMagic(array) {
+    let presum = 0, sum = 0
+    for (let i = 0; i < array.length; i++) {
+        let sum = 0
+        for (let j = 0; j < array[0].length; j++) {
+            sum += array[i][j];
+        }
+
+        if (i === 0) {
+            presum = sum;
+        }
+        if (presum != sum) {
+            return false;
+        }
+
+        presum = sum;
+
+
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        let sum = 0
+        for (let j = 0; j < array[0].length; j++) {
+            sum += array[j][i];
+        }
+
+        if (i === 0) {
+            presum = sum;
+        }
+        if (presum != sum) {
+            return false;
+        }
+
+        presum = sum;
+
+
+    }
+    return true;
+}
+
+
+// default ans;
+
+function magicSquare(arr) {
+
+    var N = arr.length
+
+
+    var sumd1 = 0, sumd2 = 0;
+
+    for (var i = 0; i < N; i++) {
+
+
+        sumd1 += arr[i][i];
+
+        sumd2 += arr[i][N - 1 - i];
+
+    }
+
+
+    if (sumd1 != sumd2)
+
+        return false;
+
+
+
+
+    for (var i = 0; i < N; i++) {
+
+        var colSum = 0;
+
+        var rowSum = 0;
+
+        for (var j = 0; j < N; j++) {
+
+            rowSum += arr[i][j];
+
+            colSum += arr[j][i];
+
+        }
+
+        if (rowSum != colSum || colSum != sumd1)
+
+            return false;
+
+    }
+
+    return true;
+
+}
+
+
+var arr = [[2, 7, 6],
+
+[9, 5, 1],
+
+[4, 3, 8]];
+
+
+if (magicSquare(arr))
+
+    console.log("Array is Magic Square");
+
+else
+
+    console.log("Array is not a Magic Square");
