0a0696e1bda8e3d538233862dad520882a2cfe48
Aman burnwal <aman1@mailfence.com>
2022-07-24 15:48:42 +0530

    mcq

---
+++ Practice In JavaScript Progrmas/mcq.js
@@ -0,0 +1,81 @@
+/**
+ * MCQ
+
+1) What will be the output array when we rotate this array by 2?
+ Input -> [2,3,4,5,6,7,8]
+A) 4,5,6,7,8,3,2 (correct ans)
+B) 3,4,5,6,7,8,2
+C) 5,6,7,2,3,4,8
+D) None
+
+2) When we rotate an array using its elements one by one, it is an in-place algorithm
+A) True [Correct Answer]
+B) False
+
+3) Which statement is true about Arrays?
+A) Arrays are immutable
+B) Arrays are not linear data structure   [correct answer]
+C) Arrays are used to store similar datatypes
+D) None
+
+
+4) Which statement is not an advantage forArrays?
+A) Stack can be implemented using Arrays
+B) Queue can be implemented using Arrays
+C) Memory inefficient [correct answer]
+D) Indexing
+
+
+5) When we delete an element from the index which is not present in the array, which condition is that? 
+A) Overflow
+B) Underflow   [correct answer]
+C) Garbage
+D) None
+
+6. Which of the below code will give us the length of an array arr?
+a) arr.length [correct answer]
+b) arr.len
+c) length(arr)
+d) None of the above.
+
+
+7. What is the functionality of the push method in an array?
+a) Insert another element in the array  [correct answer]
+b) increases the length of array by one
+c) returns the deleted element
+d) deletes the first element of array
+
+
+
+8. What is the output of the following code?
+
+var input = [1,2,4,5,6, 7];
+input.filter(x => x>3);
+console.log(input);
+
+a) 1,2,3,4,5,6  [correct answer]
+b) 4,5,6,7
+c) 1,2,3
+d) Error
+
+
+9. What will be the result of below code?
+
+var input = ['h','e','l','l','o'];
+input.join('');
+
+a) 'hello'
+b) hello            [correct answer]
+c) 'helo'
+d) None of the above
+
+
+5. What is the output of the following code?
+var a = [1,2,4,5,6, 7];
+console.log(a.splice(0,4));
+a) [1,2, 4, 5]      [correct answer]
+b) [5,6]
+c) [2,4]
+d) [1,2,4]
+ 
+ */
