6e4ecb8162f2b10f288e864ce4d3ce5699c815d9
Aman burnwal <aman1@mailfence.com>
2022-07-28 21:54:24 +0530

    create 2D array

---
+++ ReLevel By Unacademy/Class Desk/8June/create a 2D array.js
@@ -0,0 +1,9 @@
+// Way to create a 2D array
+//3 rows and 4 columns -> 3 x 4
+let matrix = new Array(3); //[]
+
+for (let i = 0; i < 3; i++) {
+    matrix[i] = new Array(4);
+}
+
+console.log(matrix);
