d541e87c702bf33a50fc3dbe6a1a55a8fcea7fef
Aman burnwal <aman1@mailfence.com>
2022-07-29 11:15:01 +0530

    for each in 2D array

---
+++ ReLevel By Unacademy/Class Desk/8June/foreach.js
@@ -0,0 +1,10 @@
+//For each loop in a 2D array
+let arr = [
+    [3, 4, 5, 6], // 0th index
+    [4, 1, -2, 0], // 1st index
+    [2, 9, 0, 6]  // 2nd index
+  ]
+  
+  
+  arr.forEach(ele => ele.forEach(x => console.log(x)))
+  
