a776e7901df10a403c51e1ab53b7ba28a0ad5fed
Aman burnwal <aman1@mailfence.com>
2022-07-31 01:36:50 +0530

    some important notes added

--- ReLevel By Unacademy/notes_for_javascript_with_code/basic.js
+++ ReLevel By Unacademy/notes_for_javascript_with_code/basic.js
@@ -1,3 +1,29 @@
 //JavaScript improves the user interface of the webpage by converting user static web page to an interactive one.
 // we use .js extension for java script
 // we can use let and var both for declaration of a variable.
+
+
+
+// order of precendence [when both variable name and function names are same]
+
+// 1. variable assignment takes precedence over function declaration;
+    /**
+     * var name = "Aman";
+     * 
+     * function name () {
+     *      console.log("Aman");
+     * }
+     * 
+     * console.log(typeof(name))    // here output will be string
+     */
+
+// 2. function declararion take precedence over variable declaration
+    /**
+     * var name;
+     * 
+     * function name() {
+     *    console.log("Aman")
+     * }
+     * 
+     * console.log(typeof(name));   // here output will be function
+     */
