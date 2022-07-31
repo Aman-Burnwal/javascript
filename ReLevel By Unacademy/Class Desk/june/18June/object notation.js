3b6e5ca7d89b3731bfe5dfff4b6a676015654f1d
Aman burnwal <aman1@mailfence.com>
2022-07-31 22:16:56 +0530

    object notation

---
+++ ReLevel By Unacademy/Class Desk/june/18June/object notation.js
@@ -0,0 +1,15 @@
+// Implement the mutation using object in javascript.
+// Given object 
+const obj = {
+    Javascript: 'hard',
+    Java: 'easy',
+    Python: 'medium',
+}
+// Change property of Javascript from ‘hard’ to ‘easy’
+
+
+let obj1 = obj;
+
+obj1.Java = 'hard';
+
+console.log(obj.Java);
