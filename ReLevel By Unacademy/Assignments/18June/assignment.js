f6723109a08b4ad61a2fc575ae4ed531e0429f97
Aman burnwal <aman1@mailfence.com>
2022-07-31 22:16:08 +0530

    assignment

---
+++ ReLevel By Unacademy/Assignments/18June/assignment.js
@@ -0,0 +1,6 @@
+// 1) Write a program to multiply the value in the given array and return a result (use array functions)
+
+
+let array = [1, 2, 3, 4, 5, 6, 7];
+
+array.forEach((x) => console.log(x*x))
---
+++ ReLevel By Unacademy/Assignments/18June/assignment2.js
@@ -0,0 +1,23 @@
+// 2) Write a JavaScript program to sort by id an array of JavaScript objects.
+
+
+const objects = [
+    {
+        Id: 45,
+        userName: "Aman"
+
+    },
+
+    {
+        Id: 22,
+        userName: "Banti"
+    },
+
+    {
+        Id: 14,
+        userName: "Jeevendra"
+    }
+    
+];
+
+console.log(objects);
