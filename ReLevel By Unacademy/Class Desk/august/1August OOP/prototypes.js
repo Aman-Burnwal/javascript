9491dfcc10abfe3ffd38cce06b11d33b5646ff7a
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2022-08-03 11:05:39 +0530

    prototypes

--- ReLevel By Unacademy/Class Desk/august/1August OOP/prototypes.js
+++ ReLevel By Unacademy/Class Desk/august/1August OOP/prototypes.js
@@ -1,15 +1,17 @@
 //constructor function
-function Person() {
-    this.name = "John";
-    this.age = 23
+class Person {
+    constructor() {
+        this.name = "John";
+        this.age = 23;
+    }
+    greet() {
+        console.log(`Hello, ${this.name}`);
+    }
 }
 
 let person1 = new Person();
 
 
-Person.prototype.greet = function () {
-    console.log(`Hello, ${this.name}`)
-}
 
 Person.prototype.department = "Computer Science"
 
@@ -18,3 +20,17 @@ let person2 = new Person();
 person1.greet()
 console.log(person1)
 console.log(person2)
+
+
+// prototype cahining
+
+class prototypeDemo {
+    constructor(Name) {
+        this.Name = Name;
+    }
+}
+
+var obj = new prototypeDemo("Aman");
+console.log(obj)
+
+console.log(Object.getPrototypeOf(obj));
