feb982e0945f65deabded6e68d2380c63d32afcf
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2022-08-04 14:47:52 +0530

    sum of diogonal elements

---
+++ Practice In JavaScript Progrmas/august/day36_03August/sum of diogonal elements.js
@@ -0,0 +1,23 @@
+// Given a 2D array, find the sum of the diagonal and the boundary elements of it.
+// In the given matrix of size 5x5 the colored cell marks the diagonal and the boundary elements
+A=[
+    [1,  2,  3,  4,  1 ],
+    [5,  6,  7,  8,  2 ],
+    [9,  10, 11, 12, 13],
+    [13, 14, 15, 16, 15],
+    [11, 12, 15, 19, 15],
+]
+
+
+// function sum (arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = 0; j < arr[0].length; j++) {
+//             if(i== 0 || j === 0 || i === arr.length - 1 || j === arr[0].length - 1) {
+//                 sum += arr[i][j]
+//             } else if ()
+
+
+//         }
+//     }
+// }
