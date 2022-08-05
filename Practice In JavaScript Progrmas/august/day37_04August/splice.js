80c63411349fc98fabd0b874192e4ba60032b3dd
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2022-08-05 18:19:13 +0530

    splice

---
+++ Practice In JavaScript Progrmas/august/day37_04August/splice.js
@@ -0,0 +1,12 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+fruits.splice(2, 0, "Lemon", "Kiwi");
+
+var a = [1, 2, 3, 4, 5]
+
+
+// a.splice(0, 3); // 45
+console.log(a.splice(0, 3));
+
+console.log(a);
+
