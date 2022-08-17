fafae6965710297aca0915974a2fba874ff5775f
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2022-08-17 12:47:10 +0530

    find element times

---
+++ Practice In JavaScript Progrmas/august/day45_16August/find element persence times.js
@@ -0,0 +1,26 @@
+// KinaSonaTujhyRabNeyBnaya 
+
+
+let word = "KinaSonaTujhyRabNeyBnaya";
+// console.log(word.split("").join(" "))
+
+// word = word.split("").join(" ");
+
+
+let 
+
+
+
+// wordCount(word);
+function wordCount(word) {
+    let count = 0;
+
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] == 'a') {
+            count++;
+        }
+
+    }
+
+    console.log(count);
+}
