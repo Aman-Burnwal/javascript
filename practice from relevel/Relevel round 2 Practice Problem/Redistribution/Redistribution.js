e5ecebadd9c83e19fc358349e3039b4f7e21c286
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2022-10-04 16:01:50 +0530

    practice

--- practice from relevel/Relevel round 2 Practice Problem/Redistribution/Redistribution.js
+++ practice from relevel/Relevel round 2 Practice Problem/Redistribution/Redistribution.js
@@ -1,1 +1,28 @@
-console.log("Solution ")
+const mod = 1000000007
+
+let currSum = 0;
+
+function findAllPossibleSolutionCount (currSum) {
+    if(currSum === n) return 1;
+
+    if(currSum > n) return 0;
+
+    if(dp[currSum] !== -1) {
+        return dp[currSum];
+    }
+
+    let possibleSolutionCount = 0;
+
+    for(let i = 3; i <= n; i + 2) {
+        possibleSolutionCount += findAllPossibleSolutionCount(currSum + 1);
+    }
+
+    dp[currSum] = possibleSolutionCount % mod;
+    return possibleSolutionCount % mod
+
+}
+
+
+var n = 890;
+let dp = Array(n).fill(n - 1);
+console.log(findAllPossibleSolutionCount(currSum))
