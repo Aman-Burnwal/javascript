1bed5367cc9873eb635493f0d0223038ed60e1ac
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2022-10-10 10:28:18 +0530

    optimized solution

--- leet Code/27. Remove Element.js
+++ leet Code/27. Remove Element.js
@@ -83,6 +83,27 @@ var removeElement = function (nums, val) {
 
 };
 
-nums = [0, 1, 2, 2, 3, 0, 4, 2], val = 2
 
-console.log(removeElement(nums, val))
+
+// soulution 2 more optimized
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+
+
+    while (nums.includes(val)) {
+        nums.splice(nums.indexOf(val), 1);
+    }
+
+
+
+};
+
+nums = [0, 1, 2, 2, 3, 0, 4, 2], val = 2
+removeElement(nums, val)
+console.log(nums)
