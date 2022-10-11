30d7ebe714cf1294bd7f82413d91260023ff5445
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2022-10-11 12:09:43 +0530

    valid anagram

---
+++ leet Code/242. Valid Anagram.js
@@ -0,0 +1,49 @@
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+
+
+// Example 2:
+
+// Input: s = "rat", t = "car"
+// Output: false
+
+
+// Constraints:
+
+// 1 <= s.length, t.length <= 5 * 104
+// s and t consist of lowercase English letters.
+
+
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+
+    if (s.length !== t.length) return false;
+
+    s = s.split('').sort().join('');
+    t = t.split('').sort().join('');
+
+    return s.includes(t);
+
+    
+
+
+};
+
+
+// let s = "anagramanagramanagramanagramanagramnagaramnagaram", t = "nagaramnagaramnagaramnagaramnagaramnagaramnagaram";
+
+// console.log(isAnagram(s, t))
+
