6fb7d1ab6b627d1e86ebc5eabc880f2893554617
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2022-10-11 12:53:48 +0530

    1342. Number of Steps to Reduce a Number to Zero

---
+++ leet Code/49. Group Anagrams.js
@@ -0,0 +1,40 @@
+// Given an array of strings strs, group the anagrams together.You can return the answer in any order.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+
+// Example 1:
+
+// Input: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+// Output: [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
+// Example 2:
+
+// Input: strs = [""]
+// Output: [[""]]
+// Example 3:
+
+// Input: strs = ["a"]
+// Output: [["a"]]
+
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+
+    if (strs.length === 0 || strs.length === 1) return [strs]
+
+    const store = {};
+
+
+    
+};
+
+
+function isAnagram()
+
+const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+
+console.log(groupAnagrams(strs));
