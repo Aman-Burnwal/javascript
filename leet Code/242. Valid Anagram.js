069048ff8a5a56c4f8407d7805ad1528bcb98f9e
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2022-10-11 12:26:47 +0530

    using hashmap

--- leet Code/242. Valid Anagram.js
+++ leet Code/242. Valid Anagram.js
@@ -23,27 +23,62 @@
 
 
 
+
+
+
+// solution 1
+// using hasmap
+
+
+// Intuition
+// Since we want to prioritize time complexity over space complexity we can use a HashMap / HashTable which will take up some space but will allow us to come up with an O(n) solution.
+
+//     Approach
+// Since the inputs are strings we can use a hash to track the count of all the letters in either s or t.Once we have a count of all the letters in one of the strings we can then loop over the other string and decrement the count until we encounter a negative count for that letter or completely finish the second loop.
+
+//     Complexity
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+
 /**
  * @param {string} s
  * @param {string} t
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-
     if (s.length !== t.length) return false;
-
-    s = s.split('').sort().join('');
-    t = t.split('').sort().join('');
-
-    return s.includes(t);
-
     
+    const store = {};
+    
+    for (let i = 0; i < s.length; i++) {
+        if (store[s[i]] > 0) {
+            store[s[i]] += 1;
+        } else {
+            store[s[i]] = 1;
+        }
+    }
+
+    for (let j = 0; j < t.length; j++) {
+        if (store[s[j]] > 0) {
+            store[s[j]] -= 1;
+        } 
+
+        if (store[s[j]] < 0) {
+            return false;
+        }
+    }
+
+    return true;
 
 
 };
 
 
-// let s = "anagramanagramanagramanagramanagramnagaramnagaram", t = "nagaramnagaramnagaramnagaramnagaramnagaramnagaram";
+let s = "anagramanagramanagramanagramanagramnagaramnagaram", t = "nagaramnagaramnagaramnagaramnagaramnagaramnagaram";
 
-// console.log(isAnagram(s, t))
+console.log(isAnagram(s, t))
+
+
+
 
