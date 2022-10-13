8751caff2d62a681d68e18923527c75db0f2f6d1
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2022-10-13 12:53:00 +0530

    assignment 2 solved

--- ReLevel By Unacademy/Advance DSA/11 Oct Advanced Problem Solving Arrays and Objects/Assignment/Assignment 2.js
+++ ReLevel By Unacademy/Advance DSA/11 Oct Advanced Problem Solving Arrays and Objects/Assignment/Assignment 2.js
@@ -3,6 +3,22 @@
 
 function rearrange(arr, left, right) {
 
-    
+    while (left!== right) {
+        if (arr[left] >= 0 && arr[right] < 0) {
 
+            [arr[left], arr[right]] = [arr[right], arr[left]]
+        } else if (arr[left] < 0) {
+            left++;
+        } else if (arr[right] >= 0) {
+            right--;
+        }
+    }
+    return arr;
 }
+
+
+const arr = [- 5, 3, -4, 88, -9, -10, 21, 5, 6];
+
+rearrange(arr, 0, arr.length - 1);
+
+console.log(arr);
