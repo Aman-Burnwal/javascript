ee8663b5becf75f61e81fcbd7f56c4d7e060f45f
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2022-10-15 14:06:04 +0530

    2011. Final valur of Variable After Operations

---
+++ leet Code/2011. Final Value of Variable After Performing Operations.js
@@ -0,0 +1,77 @@
+/**
+ * question link https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+ */
+
+/**
+ * 
+There is a programming language with only four operations and one variable X:
+
+++X and X++ increments the value of the variable X by 1.
+--X and X-- decrements the value of the variable X by 1.
+Initially, the value of X is 0.
+
+Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+
+ 
+
+Example 1:
+
+Input: operations = ["--X","X++","X++"]
+Output: 1
+Explanation: The operations are performed as follows:
+Initially, X = 0.
+--X: X is decremented by 1, X =  0 - 1 = -1.
+X++: X is incremented by 1, X = -1 + 1 =  0.
+X++: X is incremented by 1, X =  0 + 1 =  1.
+Example 2:
+
+Input: operations = ["++X","++X","X++"]
+Output: 3
+Explanation: The operations are performed as follows:
+Initially, X = 0.
+++X: X is incremented by 1, X = 0 + 1 = 1.
+++X: X is incremented by 1, X = 1 + 1 = 2.
+X++: X is incremented by 1, X = 2 + 1 = 3.
+Example 3:
+
+Input: operations = ["X++","++X","--X","X--"]
+Output: 0
+Explanation: The operations are performed as follows:
+Initially, X = 0.
+X++: X is incremented by 1, X = 0 + 1 = 1.
+++X: X is incremented by 1, X = 1 + 1 = 2.
+--X: X is decremented by 1, X = 2 - 1 = 1.
+X--: X is decremented by 1, X = 1 - 1 = 0.
+ 
+
+Constraints:
+
+1 <= operations.length <= 100
+operations[i] will be either "++X", "X++", "--X", or "X--".
+
+*/
+
+
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var finalValueAfterOperations = function (operations) {
+
+    let x = 0;
+
+    for (let index = 0; index < operations.length; index++) {
+
+        if (operations[index].includes("++X") || operations[index].includes("X++")) {
+            x++;
+        } else {
+            x--;
+        }
+    }
+    return x;
+
+};
+
+const operations = ["--X", "++X", "X++"];
+
+console.log(finalValueAfterOperations(operations));
