a27fff7b7d7a1fc1427d457fc1a9a2e7f87c6f61
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2022-11-01 19:37:38 +0530

    first and last element

---
+++ ReLevel By Unacademy/Advance DSA/28 Oct BTS/First and Last position of an element in a sorted array.js
@@ -0,0 +1,16 @@
+// First and Last position of an element in a sorted array
+
+
+function findElement(arr, x) {
+    let left = 0;
+    let right = arr.length - 1;
+
+
+    while (arr <= right) {
+
+        
+        
+    }
+}
+
+const arr = [1, 2, 3, 3, 8, 9, 9, 10, 10, 10, 11];
