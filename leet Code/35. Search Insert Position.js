eb1ac0c9672e61e0b88ca2e72233521097c41a88
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2022-11-03 10:00:54 +0530

    using binary seach

--- leet Code/35. Search Insert Position.js
+++ leet Code/35. Search Insert Position.js
@@ -37,15 +37,26 @@ var searchInsert = function (nums, target) {
     //     return nums.indexOf(target)
     // }
     
-    let i = 0;
-    for ( i; i < nums.length; i++) {
-        if (nums[i] >= target) {
+    if (nums[nums.length - 1] < target) return nums.length;
+    if (nums[0] > target) return 0;
+    let left = 0, right = nums.length - 1;
 
-            return i;
+    while (left <= right) {
+
+        let mid = Math.floor(left + (right - left) / 2);
+
+        if (nums[mid] === target ) {
+            return mid;
         }
-    }
 
-    return nums.length ;
+        if (nums[mid] > target && nums[mid - 1] < target) {
+            return mid;
+        }
+
+        
+
+        nums[mid] < target ? left = mid + 1 : right = mid - 1;
+    }
 
 
 
@@ -53,7 +64,7 @@ var searchInsert = function (nums, target) {
 };
 
 
-nums = [1, 3, 5, 6], target = 4
+nums = [1, 3], target = 2
 
 console.log(searchInsert(nums, target));
 
