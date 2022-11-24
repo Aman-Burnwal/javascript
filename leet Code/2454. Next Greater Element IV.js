e460793ad6c1a579dc207c9ed54631f817ca9103
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2022-11-24 13:44:06 +0530

    next greater element

--- leet Code/2454. Next Greater Element IV.js
+++ leet Code/2454. Next Greater Element IV.js
@@ -3,33 +3,38 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var secondGreaterElement = function (nums) {
+// var secondGreaterElement = function (nums) {
 
-    let ans = [];
-    for (let i = 0; i < nums.length - 2; i++) {
+//     // let ans = [];
+//     // for (let i = 0; i < nums.length - 2; i++) {
 
-        let count = 0;
-        let j = i + 1;
-        let found = false;
-        for (j; j < nums.length; j++) {
+//     //     let count = 0;
+//     //     let j = i + 1;
+//     //     let found = false;
+//     //     for (j; j < nums.length; j++) {
 
-            if (nums[i] < nums[j]) count++;
+//     //         if (nums[i] < nums[j]) count++;
 
-            if (count === 2) {
-                found = true;
-                break;
-            }
-        }
-        // console.log(found)
-        let temp = nums[j]
-        found ? ans[i] = temp : ans[i] = -1;
-    }
-    ans.push(-1);
-    ans.push(-1);
-    return ans;
+//     //         if (count === 2) {
+//     //             found = true;
+//     //             break;
+//     //         }
+//     //     }
+//     //     // console.log(found)
+//     //     let temp = nums[j]
+//     //     found ? ans[i] = temp : ans[i] = -1;
+//     // }
+//     // ans.push(-1);
+//     // ans.push(-1);
+//     // return ans;
 
-};
+// };
 // [1, 17, 18, 0, 18, 10, 20, 0]
 // [18, 20,  0, 20,  0,  0, -1, -1]
 // [18, 18, -1, 10, -1, -1, -1, -1]
+
+var secondGreaterElement = function (nums) {
+
+
+}
 console.log(secondGreaterElement([1, 17, 18, 0, 18, 10, 20, 0]))
