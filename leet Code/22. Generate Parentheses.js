b5818ca0a20dec7116001dbd08ffafe9e0002bc0
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2022-12-08 09:34:21 +0530

    generate paranthesis

---
+++ leet Code/22. Generate Parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+const generateParenthesis = (n) => {
+
+    let ans = []
+    const go = (start, end, s) => {
+
+        if (s.length === 2 * n) {
+            ans.push(s);
+            return;
+        }
+
+        if (start < n) go(start + 1, end, s + "(");
+        if (end < start) go(start, end + 1, s + ")");
+    }
+
+    go(0, 0, "")
+
+    return ans;
+};
+
+console.log(generateParenthesis(3))
