bb4adec27ade3f0303e2c99036f0415bfa1e0a76
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2022-12-10 11:16:28 +0530

    algorithm analysis

--- temp.js
+++ temp.js
@@ -1,34 +1,24 @@
-function binarysearch(arr, n, k) {
-  // code here
-  let start = 0;
-  let end = n - 1;
-  let mid = 0; 
+
+function solution (n) {
 
 
-  while (start <= end) {
+  for (i = 1; Math.pow(i, 3) <= n; i++) {
 
-    mid = parseInt((start + end) / 2);
+    for (j = n; j > 1; j = Math.floor(j / 3)) {
 
-    // console.log(mid)
+      for (k = 1;  Math.pow(k, 2) <= Math.pow(n, 5); k = k + 1) {
 
-    if (arr[mid] === k) {
-
-      return mid;
-
-    } else if (arr[mid] < k) {
-
-      start = mid + 1;
-      
-
-    } else if (arr[mid] > k) {
-      end = mid - 1;
-      
+        console.log("a")
+      }
+      console.log("break")
     }
   }
-  return;
+
+
 }
 
-N = 5
-arr = [1, 2, 3, 4, 5];
-k = 4;
-console.log(binarysearch(arr, N, k))
+solution(2)
+
+
+
+
