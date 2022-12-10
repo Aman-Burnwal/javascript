85ce7edc6d4856965efc1379c0f0d1f28b8e8b4d
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2022-12-10 14:03:17 +0530

    only theory

---
+++ ReLevel By Unacademy/Advance DSA/10 dec BT/Breadth first search.js
@@ -0,0 +1,6 @@
+/**
+ * @Breadth @first @search -> start from the root node & 
+ * visits all the nodes of the current level before moving to the next level.
+ * 
+ * it is also known as level order traversal
+ */
