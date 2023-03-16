11ec7fde3e51e21d2716aa6bb7918d705e3b5d89
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2023-03-16 12:27:14 +0530

    with optimized solution

--- hackerrank challenge/MountBlue Job Challenge/Electronics Shop.js
+++ hackerrank challenge/MountBlue Job Challenge/Electronics Shop.js
@@ -18,3 +18,33 @@ function getMoneySpent(keyboards, drives, b) {
     return max;
 
 }
+
+
+function getMoneySpent(keyboards, drives, b) {
+    keyboards.sort((a, b) => b - a);
+    drives.sort((a, b) => b - a);
+
+    let max = -1;
+
+    for (let i = 0; i < keyboards.length; i++) {
+        for (let j = 0; j < drives.length; j++) {
+            let sum = keyboards[i] + drives[j];
+
+            if (sum > b) {
+                // The sum is too large, so break out of the inner loop
+                break;
+            }
+
+            if (sum > max) {
+                max = sum;
+            }
+        }
+
+        if (keyboards[i] > b) {
+            // The keyboard price is too large, so break out of the outer loop
+            break;
+        }
+    }
+
+    return max;
+}
